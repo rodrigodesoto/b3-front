e4ccc90335194b056b96d30320187d738dcfef41
Soto <rodrigo.soto@airesdigital.com.br>
2024-09-09 22:41:15 -0300

    Tratamento de erros e inserção do campo order

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -8,31 +8,47 @@ import { EmailValidator } from '@angular/forms';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if ([401, 403].includes(err.status) && this.accountService.accountValue) {
-                // auto logout if 401 or 403 response returned from api
-                this.accountService.logout();
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+      if ([401, 403].includes(err.status) && this.accountService.accountValue) {
+        // auto logout if 401 or 403 response returned from api
+        this.accountService.logout();
+      }
+
+      let error = (err && err.error && err.error.message) || err.statusText;
+
+      if (err.status == 0) {
+        error = 'Sem comunicação com backend!';
+      } else if (err.status == 400) {
+
+        const obj = err.error;
+
+        for (const chave in obj) {
+          if (obj.hasOwnProperty(chave)) {
+            const objErr = obj[chave];
+
+            if(objErr.toString().substring(0,95).search('duplicate key error') != -1) {
+              error = error + '\n' +objErr.toString().substring(0,95);
             }
+          }
+        }
 
-            let error = (err && err.error && err.error.message) || err.statusText;
-
-            if(err.status == 0){
-              error = 'Sem comunicação com backend!';
-            } else if (err.status == 500 && err.error.message.toString().search('Email ou senha está incorreto') != -1) {
-              error = 'Email ou senha está incorreto!';
-            } else if (err.status == 500 && err.error.message.toString().search('já está registrado') != -1) {
-              error = 'Email já está registrado!';
-            } else if (err.status == 500 && err.error.message.toString().search('Invalid login')  != -1
-              && err.error.message.toString().search('support@') != -1) {
-              error = 'Não foi possível enviar email!';
-            } else {
-              let msgErro = error;
-              error = 'Erro: \n'+msgErro;
-            }
-            return throwError(error);
-        }))
-    }
+      } else if (err.status == 401) {
+        error = err.statusText;
+      } else if (err.status == 500 && err.error.message.toString().search('Email ou senha está incorreto') != -1) {
+        error = 'Email ou senha está incorreto!';
+      } else if (err.status == 500 && err.error.message.toString().search('já está registrado') != -1) {
+        error = 'Email já está registrado!';
+      } else if (err.status == 500 && err.error.message.toString().search('Invalid login') != -1
+        && err.error.message.toString().search('support@') != -1) {
+        error = 'Não foi possível enviar email!';
+      } else {
+        let msgErro = error;
+        error = 'Erro: \n' + msgErro;
+      }
+      return throwError(error);
+    }))
+  }
 }
